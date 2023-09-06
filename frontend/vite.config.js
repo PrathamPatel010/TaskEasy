bd5907ec44bfa23eb1688ede21cdb3d284fe6582
Pratham <ppratham812@gmail.com>
2023-09-06 16:53:16 +0530

    made changes for deployment as PWA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,47 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
+const manifestForPlugin = {
+    registerType: 'prompt',
+    includeAssets: ['vite.svg', "apple_icon_1.png", "maskable_icon.png"],
+    manifest: {
+        name: "TaskEasy",
+        short_name: "TaskEasy",
+        description: "A Simple Todo App",
+        icons: [{
+                src: '/android_x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+            },
+            {
+                src: '/android_x512',
+                sizes: '512x512',
+                type: 'image/png',
+            },
+            {
+                src: '/maskable_2.png',
+                sizes: '180x180',
+                type: 'image/png',
+                purpose: 'apple touch icon',
+            },
+            {
+                src: '/maskable_icon.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'any maskable',
+            }
+        ],
+        theme_color: '#171717',
+        background_color: '#f0e7db',
+        display: "standalone",
+        scope: '/',
+        start_url: "/",
+        orientation: 'portrait'
+    }
+}
+
 export default defineConfig({
-    plugins: [react()],
+    base: "./",
+    plugins: [react(), VitePWA(manifestForPlugin)],
 })
